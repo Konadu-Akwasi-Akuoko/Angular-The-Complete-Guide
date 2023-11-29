dced58f9ece81db60a4bed1e91db2027baf2c4b5
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-11-29 08:57:14 +0000

    feat: done with assignment 2 in the 'the basics' section

--- the-basics/basics-assignment-2-start/src/app/app.component.ts
+++ the-basics/basics-assignment-2-start/src/app/app.component.ts
@@ -3,7 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  username = '';
+  buttonDisabled = true;
+
+  onResetButtonClicked() {
+    this.username = '';
+    this.buttonDisabled = true;
+  }
+
+  onUsernameInputChanged(event: Event) {
+    if ((<HTMLInputElement>event.target).value) {
+      this.buttonDisabled = false;
+    } else {
+      this.buttonDisabled = true;
+    }
+  }
 }
