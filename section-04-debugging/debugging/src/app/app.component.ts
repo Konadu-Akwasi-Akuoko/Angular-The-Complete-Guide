ecc6bd2e485ad5d2d6180ea0ffd51cd0747e1b02
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2023-12-14 16:12:55 +0000

    feat: fix server list rendering issue in section 04 of the course

--- section-04-debugging/debugging/src/app/app.component.ts
+++ section-04-debugging/debugging/src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers;
+  servers : string[] = ['Server 1', 'Server 2'];
 
   onAddServer() {
     this.servers.push('Another Server');
   }
 
   onRemoveServer(id: number) {
-    const position = id + 1;
+    const position = id;
     this.servers.splice(position, 1);
   }
 }
