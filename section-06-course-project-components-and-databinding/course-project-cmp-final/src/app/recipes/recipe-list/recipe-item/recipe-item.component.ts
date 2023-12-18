ba57218b03c7af29a4d91d912539b4ad86afb4ac
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2023-12-18 15:01:50 +0000

    refactor: refactored the code

--- section-06-course-project-components-and-databinding/course-project-cmp-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ section-06-course-project-components-and-databinding/course-project-cmp-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
-export class RecipeItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class RecipeItemComponent {
+  @Input() recipe: Recipe;
 }
