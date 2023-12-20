08d06b4a39cbb4221efd449aa2aa5dccf76ed1bc
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2023-12-20 12:11:49 +0000

    fix: rewrote a code from ngFor to the new @For loop

--- section-09-using-services-and-dependency-injection/services-start/src/app/account/account.component.ts
+++ section-09-using-services-and-dependency-injection/services-start/src/app/account/account.component.ts
@@ -3,16 +3,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent {
-  @Input() account: {name: string, status: string};
+  @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
-
+  @Output() statusChanged = new EventEmitter<{
+    id: number;
+    newStatus: string;
+  }>();
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.statusChanged.emit({ id: this.id, newStatus: status });
     console.log('A server status changed, new status: ' + status);
   }
 }
--- section-09-using-services-and-dependency-injection/services-start/src/app/app.component.ts
+++ section-09-using-services-and-dependency-injection/services-start/src/app/app.component.ts
@@ -3,29 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   accounts = [
     {
       name: 'Master Account',
-      status: 'active'
+      status: 'active',
     },
     {
       name: 'Testaccount',
-      status: 'inactive'
+      status: 'inactive',
     },
     {
       name: 'Hidden Account',
-      status: 'unknown'
-    }
+      status: 'unknown',
+    },
   ];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
+  onAccountAdded(newAccount: { name: string; status: string }) {
     this.accounts.push(newAccount);
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+  onStatusChanged(updateInfo: { id: number; newStatus: string }) {
     this.accounts[updateInfo.id].status = updateInfo.newStatus;
   }
 }
