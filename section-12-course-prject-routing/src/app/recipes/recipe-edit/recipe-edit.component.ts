c235a537a676108c95eae40523505064ff0218b5
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-03-19 11:07:24 +0000

    feat: now the /recipes/:id/edit routes check to see if the id is in the array

--- section-12-course-prject-routing/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ section-12-course-prject-routing/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrl: './recipe-edit.component.css'
+  styleUrl: './recipe-edit.component.css',
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      let id = params['id'];
+      let recipeDetails = this.recipeService.getRecipe(id);
+
+      // If the recipe does not exist, redirect to the recipes page
+      if (!recipeDetails) {
+        this.router.navigate(['/recipes']);
+      }
+    });
+  }
 }
